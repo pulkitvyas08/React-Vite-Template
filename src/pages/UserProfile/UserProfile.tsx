b331526c922680288eaf988eb41c590095820efc
pulkitvyas08 <e19cse284@bennett.edu.in>
2022-03-11 16:35:19 +0530

    Move to Chakra UI and all bugs fixed 🎉

--- src/pages/UserProfile/UserProfile.tsx
+++ src/pages/UserProfile/UserProfile.tsx
@@ -1,15 +1,12 @@
 import React from "react";
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
 
-import { DashboardCard } from "@components/PageCard/PageCard";
+import { PageCard } from "@components/PageCard/PageCard";
 
 export const UserProfile: React.FC = () => {
     return (
-        <Box sx={{ minWidth: 275 }}>
-            <Card variant="outlined">
-                <DashboardCard mainText={"U S E R   P R O F I L E"} />
-            </Card>
-        </Box>
+        <div>
+            User Profile
+            <PageCard />
+        </div>
     );
 };
--- src/theme.ts
+++ src/theme.ts
@@ -1,20 +1,31 @@
-import { createTheme } from "@mui/material";
+import { extendTheme } from '@chakra-ui/react'
 
-export const darkTheme = createTheme({
-    palette: {
-        mode: "dark",
-        background: {
-            default: "#191919",
-            paper: "#242424",
+export const theme = extendTheme({
+  components: {
+    Button: {
+      // 1. We can update the base styles
+      baseStyle: {
+        fontWeight: 'bold', // Normally, it is "semibold"
+      },
+      // 2. We can add a new button size or extend existing
+      sizes: {
+        xl: {
+          h: '56px',
+          fontSize: 'lg',
+          px: '32px',
         },
-    },
-});
-
-export const lightTheme = createTheme({
-    palette: {
-        mode: "light",
-        background: {
-            default: "#FFFFFF",
+      },
+      // 3. We can add a new visual variant
+      variants: {
+        'with-shadow': {
+          bg: 'red.400',
+          boxShadow: '0 0 2px 2px #efdfde',
         },
+        // 4. We can override existing variants
+        solid: (props: any) => ({
+          bg: props.colorMode === 'dark' ? 'red.300' : 'red.500',
+        }),
+      },
     },
-});
+  },
+})
